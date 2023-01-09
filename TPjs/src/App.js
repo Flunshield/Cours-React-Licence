a9b3dabed4644e165c0560f3f976e6d3657769ad
Julien BERTRAND <j.bertrand.sin@gmail.com>
2023-01-09 16:21:08 +0100

    feat: Translation from French into English

--- TPjs/src/App.js
+++ TPjs/src/App.js
@@ -17,24 +17,28 @@ function App() {
 
   function findLargestNumber (numbers)
   {
-      console.log("Début Fonction findLargestNumber")
-      console.log(numbers)
-      console.log("Le plus grand nombre est : ", Math.max(...numbers))
-      console.log("Fin Fonction findLargestNumber")
-      return Math.max(...numbers) 
+    console.log("===============================================================")
+    console.log("Start Function findLargestNumber")
+    console.log(numbers)
+    console.log("The largest number is : ", Math.max(...numbers))
+    console.log("End Function findLargestNumber")
+    console.log("===============================================================")
+    return Math.max(...numbers) 
   }
 
   function sortWords (words)
   {
-    console.log("Début Fonction sortWords")
+    console.log("Start Function sortWords")
     console.log(words)
-    console.log("Les mots triés dans l'odre ", words.sort())
-    console.log("Fin Fonction sortWords")
+    console.log("Words sorted in order ", words.sort())
+    console.log("End Function sortWords")
+    console.log("===============================================================")
     return words.sort()
   }
 
   function countOccurrences (word, letter)
   {
+    console.log("Start Function countOccurrences")
     let numberLetter = 0
     for (const element of word) {
       if (element == letter)
@@ -42,78 +46,84 @@ function App() {
         numberLetter++
       }
     }
-    console.log("Début Fonction countOccurrences")
-    console.log("Notre lettre apparait : " + numberLetter + " fois.")
-    console.log("Fin Fonction countOccurrences")
+    console.log("Our letter appears : " + numberLetter + " once.")
+    console.log("End Function countOccurrences")
+    console.log("===============================================================")
     return numberLetter
   }
 
   function flattenArray (arrayOfArray)
   {
-    console.log("Début Fonction flattenArray")
-    console.log("Notre tableua initial : ", arrayOfArray)
-    console.log(arrayOfArray.flat())
-    console.log("Fin Fonction flattenArray")
+    console.log("Start Function flattenArray")
+    console.log("Our initial table : ", arrayOfArray)
+    console.log("Our transformed table : ", arrayOfArray.flat())
+    console.log("End Function flattenArray")
+    console.log("===============================================================")
     return arrayOfArray.flat()
   }
 
   function removeDupes (arrayNumber) 
   {
-    console.log("Début Fonction removeDupes")
-    console.log("Notre tableau initial : ", arrayNumber)
+    console.log("Start Function removeDupes")
+    console.log("Our initial table : ", arrayNumber)
     const set = new Set(arrayNumber)
-    console.log("Notre tableau après : ", set)
-    console.log("Fin Fonction removeDupes")
+    console.log("Our transformed table : ", set)
+    console.log("End Function removeDupes")
+    console.log("===============================================================")
     return set
   }
 
   function swapVariables  (x, y) 
   {
-    console.log("Début Fonction swapVariables")
-    console.log("AVANT : x = ", x, " et y = ", y)
+    console.log("Start Function swapVariables")
+    console.log("BEFORE : x = ", x, " et y = ", y)
     let z = x
     x = y
     y = z
-    console.log("APRES : x = ", x, " et y = ", y)
-    console.log("Fin Fonction swapVariables")
+    console.log("AFTER : x = ", x, " et y = ", y)
+    console.log("End Function swapVariables")
+    console.log("===============================================================")
   }
 
   function parseObject (objectA)
   {
-    console.log("Début Fonction parseObject")
+    console.log("Start Function parseObject")
     const monTableauObject = Object.values(objectA)
     console.log(objectA)
     console.log(monTableauObject)
-    console.log("Fin Fonction parseObject")
+    console.log("End Function parseObject")
+    console.log("===============================================================")
     return monTableauObject
   }
 
   function mergeObjects (objectA, objectB)
   {
-    console.log("Début Fonction mergeObjects")
+    console.log("Start Function mergeObjects")
     console.log("Objet A : ", objectA, "Objet B : ", objectB)
     const mergeObject = Object.assign(objectA, objectB)
-    console.log("Résulat de la fusion des deux objets : ", mergeObject)
-    console.log("Fin Fonction mergeObjects")
+    console.log("Result of merging the two objects : ", mergeObject)
+    console.log("End Function mergeObjects")
+    console.log("===============================================================")
     return mergeObject
   }
 
   function sumArray (numbers)
   {
-    console.log("Début Fonction sumArray")
+    console.log("Start Function sumArray")
     console.log("numbers[] = ",numbers)
     const initalValue = 0;
     const sumNumbers = numbers.reduce(
       (add, currentValue) => add + currentValue, initalValue);
-    console.log("Le total de numbers[] = ",sumNumbers)
-    console.log("Fin Fonction sumArray")
+    console.log("The total of numbers[] = ",sumNumbers)
+    console.log("End Function sumArray")
+    console.log("===============================================================")
     return sumNumbers
   }
 
   function sortObject (objectC)
   {
-    console.log("Début Fonction sortObject")
-    console.log("L'objet initial : ", objectC)
+    console.log("Start Function sortObject")
+    console.log("Original purpose : ", objectC)
     let a, b, c
     ({a, b, c} = objectC)
     console.log("a = ", a)
@@ -121,8 +131,9 @@ function App() {
     console.log("c : ", c)
     const tab = [a, b, c]
     const tabSort = tab.sort()
-    console.log("L'objet trié et mis sous forme de tableau : ", tabSort)
-    console.log("Fin Fonction sortObject")
+    console.log("The object sorted and tabulated : ", tabSort)
+    console.log("End Function sortObject")
+    console.log("===============================================================")
     return tab
   }
 
@@ -136,6 +147,10 @@ function App() {
   mergeObjects (objectA, objectB)
   sumArray (numbers)
   sortObject (objectC)
+
+  return (
+    <h1 className=''>Launch the browser console to view the exercises</h1>
+  )
 }
 
 export default App;
