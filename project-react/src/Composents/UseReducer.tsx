83c429c8050743bbf7f68cae309319b704cc449f
BERTRAND <jbertrand@ecole-isitech.fr>
2023-01-12 11:15:27 +0100

    fix : Uniformity of the code for useReducer

--- project-react/src/Composents/UseReducer.tsx
+++ project-react/src/Composents/UseReducer.tsx
@@ -6,41 +6,36 @@ export default function UseReducer() {
   function reducer(state: { age: number; }, action: { type: string; }) {
     switch (action.type) {
       case 'incremented_age':
+        console.log("Age avant l'ajout : ", state)
+        return { age: state.age + 1 }
 
-        console.log("age avant l'ajout : ", state)
-        return { age: state.age +1 }
-      
-        case 'decremented_age':
+      case 'decremented_age':
+        console.log("Age avant retrait : ", state)
+        return { age: state.age - 1 }
 
-        console.log("age avant retrait : ", state)
-        return { age: state.age -1 }
-        
-        default:
-
-        console.log('je suis pas entré dans un case')
-
+      default:
+        console.log('Je suis pas entré dans un case')
         return state
-
     }
   }
 
-const [state, dispatch] = useReducer(reducer, { age: 42 });
-
-console.log(state)
-return (
-  <div className="divClasseName">
-  <h1>Test du <strong>useReducer</strong></h1>
-  <p>Hello! You are {state.age}.</p>
-    <button onClick={() => {
-      dispatch({ type: 'incremented_age' })
-    }}>
-      Increment age
-    </button>
-    <button onClick={() => {
-      dispatch({ type: 'decremented_age' })
-    }}>
-      Decrement age
-    </button>
-  </div>
-);
+  const [state, dispatch] = useReducer(reducer, { age: 0 });
+  console.log(state)
+  
+  return (
+    <div className="divClasseName">
+      <h1>Test du <strong>useReducer</strong></h1>
+      <p>Hello! You are {state.age}.</p>
+      <button onClick={() => {
+        dispatch({ type: 'incremented_age' })
+      }}>
+        Increment age
+      </button>
+      <button onClick={() => {
+        dispatch({ type: 'decremented_age' })
+      }}>
+        Decrement age
+      </button>
+    </div>
+  );
 }
